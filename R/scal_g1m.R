a3a3f2aa542678bf0fc1b1f8db0db3d5083d89e1
psp6 <prasanna_samuel@hotmail.com>
2016-05-06 14:53:33 +0530

    Sample size for estimating means

---
+++ R/scal_g1m.R
@@ -0,0 +1,24 @@
+#' Calculates sample size for estimating a mean
+#'
+#' The function requires 1) desired precision 2) expected variability in outcome and 3)
+#' level of confidence as required input.
+#'
+#' @param \sigma expected variability of the outcome
+#' @param d precision or margin of the error for the outcome
+#' @param alpha level for the confidence interval
+#'
+#' @return output Sample size
+#' @keywords means, , precision
+#'
+#' @examples
+#' scal_g2p(10, 3, 0.05)
+#'
+#'
+
+
+scal_g1m<- function(sigma, d, alpha){
+  z = qnorm(1-alpha/2)
+  n = (z^2 * sigma^2)/d^2
+  round(n)
+  }
+
